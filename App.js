af633da60aad3780b344089fc4dd87d30f1fc80a
huydum22 <45447106+huydum22@users.noreply.github.com>
2020-05-13 23:45:59 +0700

    fix app.js

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Login from './src/components/Authentication/Login/login';
+import Login from './src/components/Authentication/Login/';
 export default function App() {
   return <Login />;
 }
