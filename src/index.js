c4b308b7b4b46afc234f98b91420a953e645567b
huydum22 <45447106+huydum22@users.noreply.github.com>
2020-05-23 18:29:42 +0700

    config splash screen

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Navigator from './Navigation';
+import SplashScreen from 'react-native-splash-screen';
 // import Home from './scenes/Browse';
 export default function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   // return <Home />;
   return <Navigator />;
 }
